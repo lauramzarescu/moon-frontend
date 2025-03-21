1f73e5fe36be0718f611fda38236e721b812da1f
Laurentiu Amzarescu <lamzarescu@bitdefender.com>
2025-03-21 14:11:16 +0200

    add proxy

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export const config = (() => {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || '/v1'
-  const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || '/'
+  const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || '/socket.io'
 
   return {
     BACKEND_URL,
