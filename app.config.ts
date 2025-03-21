8784a7b5f4cba2eddabcc9c473cc0d6b538f8e16
Amzarescu Laurentiu <amzarescu41@gmail.com>
2025-03-21 13:27:33 +0200

    add dynamic envs in start.sh

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,6 @@
 export const config = (() => {
-  // const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'VITE_BACKEND_URL_PLACEHOLDER'
-  // const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'VITE_SOCKET_URL_PLACEHOLDER'
-
-  const BACKEND_URL = '/v1'
-  const SOCKET_URL = '/'
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || '/v1'
+  const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || '/'
 
   return {
     BACKEND_URL,
